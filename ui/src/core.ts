b75edae4b912d314786eb886dea6da705d7cce0b
elp <prithvi.prabhu@gmail.com>
2022-03-01 11:15:40 -0800

    refactor: Formatting only

--- ui/src/core.ts
+++ ui/src/core.ts
@@ -46,14 +46,14 @@ interface Arrow<T> extends Disposable { f: Eff<T> }
 
 /** A container that holds some value, and can be observed. */
 export interface Box<T> extends Disposable {
-    (): T
-    (value: T): T
+  (): T
+  (value: T): T
 }
 
 interface Boxed<T> extends Box<T> {
-    __boxed__: boolean // marker
-    on(f: Eff<T>, o?: any): Arrow<T>
-    touch(): void
+  __boxed__: boolean // marker
+  on(f: Eff<T>, o?: any): Arrow<T>
+  touch(): void
 }
 
 type Equal<T> = (a: T, b: T) => boolean
@@ -62,33 +62,33 @@ function different<T>(_a: T, _b: T) { return false }
 export function box<T>(value?: T, equal?: Equal<T>): Box<T>;
 /** Create a Box. */
 export function box<T>(...args: any[]): Box<T> {
-    let
-        x: T = (args.length > 0 ? args[0] : undefined) as T
-    const
-        equal = args.length > 0 ? (args.length > 1 ? args[1] : null) : different,
-        arrows: Arrow<T>[] = [],
-        on = (f: Eff<T>): Arrow<T> => {
-            const a: Arrow<T> = { f, dispose: () => remove(arrows, a) }
-            arrows.push(a)
-            return a
-        },
-        dispose = (): void => { arrows.length = 0 },
-        broadcast = (t: T): void => { for (const a of arrows) a.f(t) },
-        touch = (): void => broadcast(x),
-        f = (...ys: T[]): T => {
-            if (ys.length) { // f(x) invoked
-                const y = ys[0]
-                if (!(equal ? equal(x, y) : (x === y))) broadcast(x = y) // store and broadcast new value if changed
-            } // else f() invoked
-            return x
-        }
+  let
+    x: T = (args.length > 0 ? args[0] : undefined) as T
+  const
+    equal = args.length > 0 ? (args.length > 1 ? args[1] : null) : different,
+    arrows: Arrow<T>[] = [],
+    on = (f: Eff<T>): Arrow<T> => {
+      const a: Arrow<T> = { f, dispose: () => remove(arrows, a) }
+      arrows.push(a)
+      return a
+    },
+    dispose = (): void => { arrows.length = 0 },
+    broadcast = (t: T): void => { for (const a of arrows) a.f(t) },
+    touch = (): void => broadcast(x),
+    f = (...ys: T[]): T => {
+      if (ys.length) { // f(x) invoked
+        const y = ys[0]
+        if (!(equal ? equal(x, y) : (x === y))) broadcast(x = y) // store and broadcast new value if changed
+      } // else f() invoked
+      return x
+    }
 
-    f.on = on
-    f.touch = touch
-    f.dispose = dispose
-    f.__boxed__ = true
+  f.on = on
+  f.touch = touch
+  f.dispose = dispose
+  f.__boxed__ = true
 
-    return f as Box<T>
+  return f as Box<T>
 }
 /** Is this a Box? */
 export function boxed<T>(x: any): x is Box<T> { return x && x.__boxed__ === true }
@@ -119,14 +119,14 @@ export function to(...args: any[]): Disposable { return react(true, args.slice(0
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 function react(immediate: boolean, boxen: Box<any>[], f: Function): Disposable {
-    const
-        xs = boxen as Boxed<any>[],
-        emit = () => f(...xs.map(x => x())),
-        arrows = xs.map(x => x.on(emit)),
-        dispose = () => arrows.forEach(a => a.dispose())
+  const
+    xs = boxen as Boxed<any>[],
+    emit = () => f(...xs.map(x => x())),
+    arrows = xs.map(x => x.on(emit)),
+    dispose = () => arrows.forEach(a => a.dispose())
 
-    if (immediate) emit()
-    return { dispose }
+  if (immediate) emit()
+  return { dispose }
 }
 
 /** Create a Box computed from another Box. */
@@ -137,28 +137,28 @@ export function by<A, B, C>(a: Box<A>, b: Box<B>, zip: Func2<A, B, C>): Box<B>
 export function by<A, B, C, D>(a: Box<A>, b: Box<B>, c: Box<C>, zip: Func3<A, B, C, D>): Box<B>
 /** Create a Box computed from N other Boxes. */
 export function by(...args: any[]): any {
-    if (args.length < 2) throw new Error(`invalid number of args: want 2 or more, got ${args.length}`)
-    const
-        m = args.length - 1,
-        xs = args.slice(0, m) as Boxed<any>[],
-        // eslint-disable-next-line @typescript-eslint/ban-types
-        f = args[m] as Function,
-        yB = box(f(...xs.map(x => x())))
+  if (args.length < 2) throw new Error(`invalid number of args: want 2 or more, got ${args.length}`)
+  const
+    m = args.length - 1,
+    xs = args.slice(0, m) as Boxed<any>[],
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    f = args[m] as Function,
+    yB = box(f(...xs.map(x => x())))
 
-    xs.forEach(x => x.on(_ => yB(f(...xs.map(x => x())))))
-    return yB
+  xs.forEach(x => x.on(_ => yB(f(...xs.map(x => x())))))
+  return yB
 }
 /** Watch a Box for changes and print changes to the console. */
 export function watch<T>(x: Box<T>, label?: string): Disposable {
-    // eslint-disable-next-line no-console
-    return on(x, label ? ((x: T) => console.log(label, x)) : ((x: T) => console.log(x)))
+  // eslint-disable-next-line no-console
+  return on(x, label ? ((x: T) => console.log(label, x)) : ((x: T) => console.log(x)))
 }
 
 function remove<T>(xs: T[], x: T): void { const i = xs.indexOf(x); if (i > -1) xs.splice(i, 1) }
 
 export const gensym = (prefix: S) => {
-    let k = 0
-    return () => prefix + k++
+  let k = 0
+  return () => prefix + k++
 }
 export const xid = gensym('x')
 export const isN = (x: any): x is number => typeof x === 'number'
