432200fc19d542880d0f81e6eeb8bfb047bb5216
elp <prithvi.prabhu@gmail.com>
2022-03-08 11:17:10 -0800

    wip: Add packaging

---
+++ py/h2o_nitro/__init__.py
@@ -0,0 +1,2 @@
+from h2o_nitro.core import UI, input, option
+__version__ = "0.1.0"
---
+++ py/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+from setuptools import setup
+
+
+def get_version(package):
+    path = os.path.join(package, "__init__.py")
+    init_py = open(path, "r", encoding="utf8").read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_long_description():
+    return open("README.md", "r", encoding="utf8").read()
+
+
+def get_packages(package):
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
+
+
+setup(
+    name="h2o_nitro",
+    version=get_version("h2o_nitro"),
+    url="https://nitro.h2o.ai/",
+    description="",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Prithvi Prabhu",
+    author_email="prithvi.prabhu@gmail.com",
+    packages=get_packages("h2o_nitro"),
+    python_requires=">=3.6",
+    install_requires=[
+        "msgpack>=1.0.3",
+    ],
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 2 - Pre-Alpha',
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        'Topic :: Database',
+        "Topic :: Internet :: WWW/HTTP",
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Communications :: Chat',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Widget Sets',
+        'Topic :: System :: Distributed Computing',
+    ],
+    project_urls={
+        "Source": "https://github.com/h2oai/nitro",
+        "Changelog": "https://github.com/h2oai/nitro/blob/master/CHANGELOG.md",
+    },
+)
