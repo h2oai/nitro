8a7efeefa143553ea8cdbb234c75c35d50f4257c
elp <prithvi.prabhu@gmail.com>
2022-03-10 11:33:25 -0800

    wip: Markdown styling

--- py/standalone.py
+++ py/standalone.py
@@ -13,7 +13,125 @@ def main2(ui: UI):
         counter += 1 if choice == '+' else -1
 
 
+sample_markdown = '''
+# Brawny gods just flocked up to quiz and vex him
+
+But how could we fail to see that there is an infinite regress here—if we do not accept, 
+as Kantian and post-Kantian philosophies do, a "supersensible" domain of freedom, outside
+of the world and its physical determinisms? According to them, the free subject is an absolute
+origin, a first agent capable of creating first causes, who initiates new causal chains *ex nihilo*,
+independent of the rest of the world. — Henri Atlan, *Is Science Inhuman*?
+
+## Amazingly few discotheques provide jukeboxes
+
+But how could we fail to see that there is an infinite regress here—if we do not accept, 
+as Kantian and post-Kantian philosophies do, a "supersensible" domain of freedom, outside
+of the world and its physical determinisms? According to them, the free subject is an absolute
+origin, a first agent capable of creating first causes, who initiates new causal chains *ex nihilo*,
+independent of the rest of the world. — Henri Atlan, *Is Science Inhuman*?
+
+
+### Why shouldn’t a quixotic Kazakh vampire jog barefoot?
+
+But how could we fail to see that there is an infinite regress here—if we do not accept, 
+as Kantian and post-Kantian philosophies do, a "supersensible" domain of freedom, outside
+of the world and its physical determinisms? According to them, the free subject is an absolute
+origin, a first agent capable of creating first causes, who initiates new causal chains *ex nihilo*,
+independent of the rest of the world. — Henri Atlan, *Is Science Inhuman*?
+
+#### Grumpy wizards make a toxic brew for the jovial queen
+
+But how could we fail to see that there is an infinite regress here—if we do not accept, 
+as Kantian and post-Kantian philosophies do, a "supersensible" domain of freedom, outside
+of the world and its physical determinisms? According to them, the free subject is an absolute
+origin, a first agent capable of creating first causes, who initiates new causal chains *ex nihilo*,
+independent of the rest of the world. — Henri Atlan, *Is Science Inhuman*?
+
+##### Jackie will budget for the most expensive zoology equipment
+
+But how could we fail to see that there is an infinite regress here—if we do not accept, 
+as Kantian and post-Kantian philosophies do, a "supersensible" domain of freedom, outside
+of the world and its physical determinisms? According to them, the free subject is an absolute
+origin, a first agent capable of creating first causes, who initiates new causal chains *ex nihilo*,
+independent of the rest of the world. — Henri Atlan, *Is Science Inhuman*?
+
+But how could we fail to see that there is an infinite regress here—if we do not accept, 
+as Kantian and post-Kantian philosophies do, a "supersensible" domain of freedom, outside
+of the world and its physical determinisms? According to them, the free subject is an absolute
+origin, a first agent capable of creating first causes, who initiates new causal chains *ex nihilo*,
+independent of the rest of the world. — Henri Atlan, *Is Science Inhuman*?
+
+###### But how could we fail to see that there is an infinite regress here—if we do not accept, as Kantian and post-Kantian philosophies do.
+
+According to them, the free subject is an absolute
+origin, a first agent capable of creating first causes, who initiates new causal chains *ex nihilo*,
+independent of the rest of the world. — Henri Atlan, *Is Science Inhuman*?
+
+**This is bold text**
+
+*This text is italicized*
+
+~~This was mistaken text~~
+
+**This text is _extremely_ important**
+	
+***All this text is important***
+
+> Text that is a quote
+
+Use `git status` to list all new or modified files that haven't yet been committed.
+
+Some basic Git commands are:
+```
+git status
+git add
+git commit
+```
+
+This site was built using [GitHub Pages](https://pages.github.com/).
+
+![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
+
+- George Washington
+- John Adams
+- Thomas Jefferson
+   - First nested list item
+     - Second nested list item
+
+1. James Madison
+1. James Monroe
+1. John Quincy Adams
+1. First list item
+   1. First nested list item
+     1. Second nested list item
+
+     
+- [x] #739
+- [ ] https://github.com/octo-org/octo-repo/issues/740
+- [ ] Add delight to the experience when all tasks are complete :tada:
+
+@octocat :+1: This PR looks great - it's ready to merge! :shipit:
+
+
+Here is a simple footnote[^1].
+
+A footnote can also have multiple lines[^2].  
+
+You can also use words, to fit your writing style more closely[^note].
+
+[^1]: My reference.
+[^2]: Every new line should be prefixed with 2 spaces.  
+  This allows you to have a footnote with multiple lines.
+[^note]:
+    Named footnotes will still render with numbers instead of the text but allow easier identification and linking.  
+    This footnote also has been made with a different syntax using 4 spaces for new lines.
+
+
+'''
+
+
 def main(ui: UI):
+    x = ui([sample_markdown])
     x = ui('Text field with label')
     x = ui('Text field with placeholder', placeholder='Enter text here')
     x = ui('Text field with value', value='Some text')
--- web/src/markdown.tsx
+++ web/src/markdown.tsx
@@ -21,9 +21,113 @@ const Container = styled.div`
 &>p:only-child {
   margin: 0;
 }
-/* Don't indent lists */
-&>ul, &>ol {
-  padding-left: 1rem;
+
+/* resets */
+
+h1, h2, h3, h4, h5, h6, p, blockquote, pre {
+  box-sizing: border-box;
+  margin: 0;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-weight: 400;
+}
+img {
+  max-width: 100%;
+  display: block;
+}
+
+/* customizations */
+
+h1 {
+  font-weight: 200;
+  font-size: 3rem;
+  line-height: 3.3rem;
+  margin-bottom: 0.5rem;
+}
+h2 {
+  font-weight: 300;
+  font-size: 2rem;
+  line-height: 3rem;
+}
+h3 {
+  font-size: 1.44rem;
+  line-height: 2rem;
+  margin-bottom: 0.25rem;
+}
+h4 {
+  font-size: 1.2rem;
+  line-height: 1.75rem;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+}
+h5 {
+  font-size: 1rem;
+  line-height: 1.75rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-feature-settings: 'smcp';
+}
+h6 {
+  font-size: 0.833rem;
+  line-height: 1.25rem;
+  margin-bottom: 1rem;
+}
+p, ul, ol {
+  font-size: 1.2rem;
+  line-height: 1.75rem;
+  margin-bottom: 1rem;
+  color: #242424;
+}
+ul {
+  list-style: none;
+  padding-left: 2rem;
+  text-indent: -1rem;
+}
+ul>li:before {
+  content: "—";
+  text-indent: -1rem;
+  margin-right: .5rem;
+}
+ol ol, ul ol {
+  list-style-type: lower-roman;
+}
+ul ul ol, ul ol ol, ol ul ol, ol ol ol {
+  list-style-type: lower-alpha;
+}
+/* Workaround: Markdown renderer adds superfluous p inside ol's li.*/
+ol li p { 
+  margin: 0;
+}
+blockquote {
+  padding: 0 1em;
+  border-left: 0.25em solid #ddd;
+}
+blockquote p {
+  color: #555; 
+}
+.footnotes {
+  h2 {
+    font-size: 1rem;
+    line-height: 1.75rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-feature-settings: 'smcp';
+  }
+  ol li, ol li p {
+    font-size: 1rem;
+  }
+}
+p code, li code, blockquote code {
+  padding: 0.2em 0.4em;
+  font-size: 85%;
+  margin: 0;
+  background-color: #eee;
+}
+pre {
+  padding: 1rem;
+  overflow: auto;
+  margin-bottom: 1rem;
+  background-color: #eee;
 }
 `
 
