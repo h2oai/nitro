1a386f167d5ed31caa90a566272a9515f3ce55df
elp <prithvi.prabhu@gmail.com>
2022-03-28 19:45:26 -0700

    wip: Add Flask integration example

---
+++ py/examples/nitro_flask.py
@@ -0,0 +1,33 @@
+import simple_websocket
+from flask import Flask, request
+
+# --- Nitro ---
+
+from h2o_nitro import View, box
+
+
+def main(view: View):
+    name = view(box('What is your name?', value='Boaty McBoatface'))
+    feel = view(box(f'How do you feel today, {name}?', value='intrigued'))
+    view(f'What a coincidence, {name}, I feel {feel}, too!')
+
+
+nitro = View(main, title='Hello Nitro!', caption='v1.0')
+
+# --- Flask ---
+
+app = Flask(__name__, static_folder='../../web/build', static_url_path='')
+
+
+@app.route('/nitro', websocket=True)
+def socket():
+    ws = simple_websocket.Server(request.environ)
+    try:
+        nitro.serve(ws.send, ws.receive)
+    except simple_websocket.ConnectionClosed:
+        pass
+    return ''
+
+
+if __name__ == '__main__':
+    app.run()
