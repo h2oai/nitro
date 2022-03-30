436d7210e8346360b8ef01c9871281058f474ceb
elp <prithvi.prabhu@gmail.com>
2022-03-29 17:07:33 -0700

    wip: Export AsyncView

--- py/h2o_nitro/__init__.py
+++ py/h2o_nitro/__init__.py
@@ -1,5 +1,4 @@
-from h2o_nitro.core import View, Box, BoxArrange, BoxAlign, Option, box, option, row, col, \
-    ProtocolError, ContextSwitchError, \
-    RemoteError
+from h2o_nitro.core import View, AsyncView, Box, BoxArrange, BoxAlign, Option, box, option, row, col, \
+    ProtocolError, ContextSwitchError, RemoteError
 
 __version__ = "0.1.0"
