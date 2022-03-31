94f85f079157f7991bf6ec0bc76757ba0f5d1d09
elp <prithvi.prabhu@gmail.com>
2022-03-30 21:06:46 -0700

    chore: Bump version to 0.2.0

--- py/h2o_nitro/__init__.py
+++ py/h2o_nitro/__init__.py
@@ -1,4 +1,4 @@
 from .core import View, AsyncView, Box, BoxArrange, BoxAlign, Option, box, option, row, col, \
     ProtocolError, ContextSwitchError, RemoteError, web_directory
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
