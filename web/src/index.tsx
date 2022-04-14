5a28e02ecc17dacb1e6895e827f1bb22d2a20488
elp <prithvi.prabhu@gmail.com>
2022-04-13 20:26:40 -0700

    fix: Default theme primary color is closer to indigo

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -27,7 +27,7 @@ const defaultScheme = {
   primaryFont: 'inherit',
   monospaceFont: 'inherit',
   primaryColor: '#5a64f0',
-  primaryColorName: 'purple',
+  primaryColorName: 'indigo',
   foregroundColor: '#3e3f4a',
   backgroundColor: '#ffffff',
 }
