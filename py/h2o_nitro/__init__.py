5b35f8ecaa5701c026139ab26a31cf41aa110b8b
elp <prithvi.prabhu@gmail.com>
2022-05-30 19:41:39 -0700

    chore: Bump version

--- py/h2o_nitro/__init__.py
+++ py/h2o_nitro/__init__.py
@@ -15,4 +15,4 @@
 from .core import View, AsyncView, Box, BoxArrange, BoxAlign, Option, Theme, box, option, header, row, col, \
     ProtocolError, ContextSwitchError, RemoteError, web_directory, lorem, Plugin, Script
 
-__version__ = "0.8.2"
+__version__ = "0.8.3"
