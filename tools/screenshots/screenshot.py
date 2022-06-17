3d1f4c05fb98eebd35a5d35b3cabad498261015c
elp <prithvi.prabhu@gmail.com>
2022-06-17 13:35:53 -0700

    docs: Adjust screenshot tool to capture webviews

--- tools/screenshots/screenshot.py
+++ tools/screenshots/screenshot.py
@@ -14,16 +14,20 @@ with sync_playwright() as p:
     print(f'Navigating to {url} ...')
     page.goto(url)
 
-    example_links = page.locator('[data-jump]').element_handles()
-    example_names = [link.get_attribute('data-jump') for link in example_links]
+    link_prefix = '#!docs.help_'
+    all_links = page.locator('a[href]').element_handles()
+    all_hrefs = [link.get_attribute('href') for link in all_links]
+    example_hrefs = [href for href in all_hrefs if href.startswith(link_prefix)]
+    example_names = [href[len(link_prefix):] for href in example_hrefs]
     print(f'Collected {len(example_names)} examples.')
 
-    for name in example_names:
-        if not name.endswith('_noop'):
-            print(f'Capturing {name}...')
-            page.locator(f'[data-jump={name}]').click()
-            page.wait_for_timeout(1000)
-            page.locator('[data-name=output]').screenshot(path=str(output_dir / f'{name}.png'))
-            page.locator('[data-name=contents]').click()
+    for name, href in zip(example_names, example_hrefs):
+        if name.endswith('_noop'):
+            continue
+        print(f'Capturing {name}...')
+        page.locator(f'[href="{href}"]').click()
+        page.wait_for_timeout(1000)
+        page.locator('[data-name=output]').screenshot(path=str(output_dir / f'{name}.png'))
+        page.locator('[data-name=contents]').click()
     browser.close()
     print('Done!')
