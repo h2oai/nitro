cd1e444b1283e215ca0e0fac3a37a2c4d32eee2f
elp <prithvi.prabhu@gmail.com>
2022-06-20 12:30:20 -0700

    chore: Use IP instead of localhost (Node 18.x fix)

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -15,5 +15,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-  app.use(createProxyMiddleware('/nitro', { target: 'http://localhost:5000', ws: true }))
+  app.use(createProxyMiddleware('/nitro', { target: 'http://127.0.0.1:5000', ws: true }))
 }
