f8ec3cca3ce54c565ac5a72892f35e326a9f1158
El P <prithvi.prabhu@gmail.com>
2022-07-04 11:59:14 -0700

    fix: Selected date is not reflected in Calendar

--- web/src/calendar.tsx
+++ web/src/calendar.tsx
@@ -13,38 +13,35 @@
 // limitations under the License.
 
 import { Calendar as FCalendar, DateRangeType } from '@fluentui/react';
-import React from 'react';
-import { dateToString, toDate } from './core';
+import { dateToString, signal, toDate } from './core';
 import { Labeled } from './label';
 import { BoxProps, make } from './ui';
 
 export const Calendar = make(({ box }: BoxProps) => {
   const
-    { context, value } = box,
-    defaultDate = toDate(value) ?? new Date(),
-    defaultValue = dateToString(defaultDate),
+    { context, text, mode, value, min, max, live } = box,
+    dateB = signal(toDate(value) ?? new Date()),
+    minDate = toDate(min),
+    maxDate = toDate(max),
+    dateRangeType = mode === 'week'
+      ? DateRangeType.Week
+      : mode === 'month'
+        ? DateRangeType.Month
+        : DateRangeType.Day,
     onSelectDate = (d?: Date) => {
-      context.record(dateToString(d ?? new Date()))
+      const date = d ?? new Date()
+      context.record(dateToString(date))
+      dateB(date)
     },
     render = () => {
       // TODO format string; aria-label
-      const
-        { text, mode, value, min, max } = box,
-        date = toDate(value),
-        minDate = toDate(min),
-        maxDate = toDate(max),
-        dateRangeType = mode === 'week'
-          ? DateRangeType.Week
-          : mode === 'month'
-            ? DateRangeType.Month
-            : DateRangeType.Day
       // TODO firstDayOfWeek, firstWeekOfYear customization
       // TODO pass strings for localization
       return (
         <Labeled label={text}>
           <FCalendar
             dateRangeType={dateRangeType}
-            value={date}
+            value={dateB()}
             minDate={minDate}
             maxDate={maxDate}
             isDayPickerVisible={mode !== 'month'}
@@ -56,7 +53,7 @@ export const Calendar = make(({ box }: BoxProps) => {
       )
     }
 
-  context.record(defaultValue)
+  context.record(dateToString(dateB()))
 
-  return { render }
+  return { render, dateB }
 })
