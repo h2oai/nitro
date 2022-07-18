0c3e846df6b749bc088b953d5edb8448c07cde4e
El P <prithvi.prabhu@gmail.com>
2022-07-18 13:18:53 -0700

    refactor: Simplify styling test declarations #67

--- web/src/style.test.ts
+++ web/src/style.test.ts
@@ -22,146 +22,154 @@ const compare = (spec: string, expected: React.CSSProperties) => {
   expect(actual).toStrictEqual(expected)
 }
 
-function forEach<T>(d: Dict<T>, f: (k: S, v: T) => void) {
+function loopd<T>(d: Dict<T>, f: (k: S, v: T) => void) {
   for (const k in d) f(k, d[k])
 }
 
-const cases: [string, React.CSSProperties][] = [
-  ['w-auto', { width: 'auto' }],
-  ['w-1/2', { width: '50%' }],
-  ['w-1/3', { width: '33.333333%' }],
-  ['w-2/3', { width: '66.666667%' }],
-  ['w-1/4', { width: '25%' }],
-  ['w-2/4', { width: '50%' }],
-  ['w-3/4', { width: '75%' }],
-  ['w-1/5', { width: '20%' }],
-  ['w-2/5', { width: '40%' }],
-  ['w-3/5', { width: '60%' }],
-  ['w-4/5', { width: '80%' }],
-  ['w-1/6', { width: '16.666667%' }],
-  ['w-2/6', { width: '33.333333%' }],
-  ['w-3/6', { width: '50%' }],
-  ['w-4/6', { width: '66.666667%' }],
-  ['w-5/6', { width: '83.333333%' }],
-  ['w-1/12', { width: '8.333333%' }],
-  ['w-2/12', { width: '16.666667%' }],
-  ['w-3/12', { width: '25%' }],
-  ['w-4/12', { width: '33.333333%' }],
-  ['w-5/12', { width: '41.666667%' }],
-  ['w-6/12', { width: '50%' }],
-  ['w-7/12', { width: '58.333333%' }],
-  ['w-8/12', { width: '66.666667%' }],
-  ['w-9/12', { width: '75%' }],
-  ['w-10/12', { width: '83.333333%' }],
-  ['w-11/12', { width: '91.666667%' }],
-  ['w-full', { width: '100%' }],
-  ['w-screen', { width: '100vw' }],
-  ['w-min', { width: 'min-content' }],
-  ['w-max', { width: 'max-content' }],
-  ['w-fit', { width: 'fit-content' }],
+function loop<T>(xs: T[], f: (x: T) => void) {
+  for (const x of xs) f(x)
+}
 
-  ['h-auto', { height: 'auto' }],
-  ['h-1/2', { height: '50%' }],
-  ['h-1/3', { height: '33.333333%' }],
-  ['h-2/3', { height: '66.666667%' }],
-  ['h-1/4', { height: '25%' }],
-  ['h-2/4', { height: '50%' }],
-  ['h-3/4', { height: '75%' }],
-  ['h-1/5', { height: '20%' }],
-  ['h-2/5', { height: '40%' }],
-  ['h-3/5', { height: '60%' }],
-  ['h-4/5', { height: '80%' }],
-  ['h-1/6', { height: '16.666667%' }],
-  ['h-2/6', { height: '33.333333%' }],
-  ['h-3/6', { height: '50%' }],
-  ['h-4/6', { height: '66.666667%' }],
-  ['h-5/6', { height: '83.333333%' }],
-  ['h-1/12', { height: '8.333333%' }],
-  ['h-2/12', { height: '16.666667%' }],
-  ['h-3/12', { height: '25%' }],
-  ['h-4/12', { height: '33.333333%' }],
-  ['h-5/12', { height: '41.666667%' }],
-  ['h-6/12', { height: '50%' }],
-  ['h-7/12', { height: '58.333333%' }],
-  ['h-8/12', { height: '66.666667%' }],
-  ['h-9/12', { height: '75%' }],
-  ['h-10/12', { height: '83.333333%' }],
-  ['h-11/12', { height: '91.666667%' }],
-  ['h-full', { height: '100%' }],
-  ['h-screen', { height: '100vw' }],
-  ['h-min', { height: 'min-content' }],
-  ['h-max', { height: 'max-content' }],
-  ['h-fit', { height: 'fit-content' }],
+type CSS = React.CSSProperties
+const cases: [string, CSS][] = []
+const tc = (input: S, expected: CSS) => cases.push([input, expected])
 
-  ['text-left', { textAlign: 'left' }],
-  ['text-center', { textAlign: 'center' }],
-  ['text-right', { textAlign: 'right' }],
-  ['text-justify', { textAlign: 'justify' }],
-  ['text-start', { textAlign: 'start' }],
-  ['text-end', { textAlign: 'end' }],
+tc('w-auto', { width: 'auto' })
+loopd(sizeScale, (k, v) => tc('w-' + k, { width: v }))
+tc('w-1/2', { width: '50%' })
+tc('w-1/3', { width: '33.333333%' })
+tc('w-2/3', { width: '66.666667%' })
+tc('w-1/4', { width: '25%' })
+tc('w-2/4', { width: '50%' })
+tc('w-3/4', { width: '75%' })
+tc('w-1/5', { width: '20%' })
+tc('w-2/5', { width: '40%' })
+tc('w-3/5', { width: '60%' })
+tc('w-4/5', { width: '80%' })
+tc('w-1/6', { width: '16.666667%' })
+tc('w-2/6', { width: '33.333333%' })
+tc('w-3/6', { width: '50%' })
+tc('w-4/6', { width: '66.666667%' })
+tc('w-5/6', { width: '83.333333%' })
+tc('w-1/12', { width: '8.333333%' })
+tc('w-2/12', { width: '16.666667%' })
+tc('w-3/12', { width: '25%' })
+tc('w-4/12', { width: '33.333333%' })
+tc('w-5/12', { width: '41.666667%' })
+tc('w-6/12', { width: '50%' })
+tc('w-7/12', { width: '58.333333%' })
+tc('w-8/12', { width: '66.666667%' })
+tc('w-9/12', { width: '75%' })
+tc('w-10/12', { width: '83.333333%' })
+tc('w-11/12', { width: '91.666667%' })
+tc('w-full', { width: '100%' })
+tc('w-screen', { width: '100vw' })
+tc('w-min', { width: 'min-content' })
+tc('w-max', { width: 'max-content' })
+tc('w-fit', { width: 'fit-content' })
 
-  ['m-auto', { margin: 'auto' }],
-  ['mx-auto', { marginLeft: 'auto', marginRight: 'auto' }],
-  ['my-auto', { marginTop: 'auto', marginBottom: 'auto' }],
-  ['mt-auto', { marginTop: 'auto' }],
-  ['mr-auto', { marginRight: 'auto' }],
-  ['mb-auto', { marginBottom: 'auto' }],
-  ['ml-auto', { marginLeft: 'auto' }],
+tc('h-auto', { height: 'auto' })
+loopd(sizeScale, (k, v) => tc('h-' + k, { height: v }))
+tc('h-1/2', { height: '50%' })
+tc('h-1/3', { height: '33.333333%' })
+tc('h-2/3', { height: '66.666667%' })
+tc('h-1/4', { height: '25%' })
+tc('h-2/4', { height: '50%' })
+tc('h-3/4', { height: '75%' })
+tc('h-1/5', { height: '20%' })
+tc('h-2/5', { height: '40%' })
+tc('h-3/5', { height: '60%' })
+tc('h-4/5', { height: '80%' })
+tc('h-1/6', { height: '16.666667%' })
+tc('h-2/6', { height: '33.333333%' })
+tc('h-3/6', { height: '50%' })
+tc('h-4/6', { height: '66.666667%' })
+tc('h-5/6', { height: '83.333333%' })
+tc('h-1/12', { height: '8.333333%' })
+tc('h-2/12', { height: '16.666667%' })
+tc('h-3/12', { height: '25%' })
+tc('h-4/12', { height: '33.333333%' })
+tc('h-5/12', { height: '41.666667%' })
+tc('h-6/12', { height: '50%' })
+tc('h-7/12', { height: '58.333333%' })
+tc('h-8/12', { height: '66.666667%' })
+tc('h-9/12', { height: '75%' })
+tc('h-10/12', { height: '83.333333%' })
+tc('h-11/12', { height: '91.666667%' })
+tc('h-full', { height: '100%' })
+tc('h-screen', { height: '100vw' })
+tc('h-min', { height: 'min-content' })
+tc('h-max', { height: 'max-content' })
+tc('h-fit', { height: 'fit-content' })
 
-  ['text-inherit', { color: 'inherit' }],
-  ['bg-inherit', { backgroundColor: 'inherit' }],
+loopd(sizeScale, (k, v) => tc('p-' + k, { padding: v }))
+loopd(sizeScale, (k, v) => tc('px-' + k, { paddingLeft: v, paddingRight: v }))
+loopd(sizeScale, (k, v) => tc('py-' + k, { paddingTop: v, paddingBottom: v }))
+loopd(sizeScale, (k, v) => tc('pt-' + k, { paddingTop: v }))
+loopd(sizeScale, (k, v) => tc('pr-' + k, { paddingRight: v }))
+loopd(sizeScale, (k, v) => tc('pb-' + k, { paddingBottom: v }))
+loopd(sizeScale, (k, v) => tc('pl-' + k, { paddingLeft: v }))
 
+tc('m-auto', { margin: 'auto' })
+tc('mx-auto', { marginLeft: 'auto', marginRight: 'auto' })
+tc('my-auto', { marginTop: 'auto', marginBottom: 'auto' })
+tc('mt-auto', { marginTop: 'auto' })
+tc('mr-auto', { marginRight: 'auto' })
+tc('mb-auto', { marginBottom: 'auto' })
+tc('ml-auto', { marginLeft: 'auto' })
 
-  ['border', { borderWidth: 1 }],
-  ['border-x', { borderLeftWidth: 1, borderRightWidth: 1 }],
-  ['border-y', { borderTopWidth: 1, borderBottomWidth: 1 }],
-  ['border-t', { borderTopWidth: 1 }],
-  ['border-r', { borderRightWidth: 1 }],
-  ['border-b', { borderBottomWidth: 1 }],
-  ['border-l', { borderLeftWidth: 1 }],
-]
+loopd(sizeScale, (k, v) => tc('m-' + k, { margin: v }))
+loopd(sizeScale, (k, v) => tc('mx-' + k, { marginLeft: v, marginRight: v }))
+loopd(sizeScale, (k, v) => tc('my-' + k, { marginTop: v, marginBottom: v }))
+loopd(sizeScale, (k, v) => tc('mt-' + k, { marginTop: v }))
+loopd(sizeScale, (k, v) => tc('mr-' + k, { marginRight: v }))
+loopd(sizeScale, (k, v) => tc('mb-' + k, { marginBottom: v }))
+loopd(sizeScale, (k, v) => tc('ml-' + k, { marginLeft: v }))
 
-forEach(sizeScale, (k, v) => cases.push(['w-' + k, { width: v }]))
-forEach(sizeScale, (k, v) => cases.push(['h-' + k, { height: v }]))
-forEach(sizeScale, (k, v) => cases.push(['p-' + k, { padding: v }]))
-forEach(sizeScale, (k, v) => cases.push(['px-' + k, { paddingLeft: v, paddingRight: v }]))
-forEach(sizeScale, (k, v) => cases.push(['py-' + k, { paddingTop: v, paddingBottom: v }]))
-forEach(sizeScale, (k, v) => cases.push(['pt-' + k, { paddingTop: v }]))
-forEach(sizeScale, (k, v) => cases.push(['pr-' + k, { paddingRight: v }]))
-forEach(sizeScale, (k, v) => cases.push(['pb-' + k, { paddingBottom: v }]))
-forEach(sizeScale, (k, v) => cases.push(['pl-' + k, { paddingLeft: v }]))
-forEach(sizeScale, (k, v) => cases.push(['m-' + k, { margin: v }]))
-forEach(sizeScale, (k, v) => cases.push(['mx-' + k, { marginLeft: v, marginRight: v }]))
-forEach(sizeScale, (k, v) => cases.push(['my-' + k, { marginTop: v, marginBottom: v }]))
-forEach(sizeScale, (k, v) => cases.push(['mt-' + k, { marginTop: v }]))
-forEach(sizeScale, (k, v) => cases.push(['mr-' + k, { marginRight: v }]))
-forEach(sizeScale, (k, v) => cases.push(['mb-' + k, { marginBottom: v }]))
-forEach(sizeScale, (k, v) => cases.push(['ml-' + k, { marginLeft: v }]))
-forEach(colorPalette, (k, v) => cases.push(['text-' + k, { color: v }]))
-forEach(colorPalette, (k, v) => cases.push(['bg-' + k, { backgroundColor: v }]));
+tc('bg-inherit', { backgroundColor: 'inherit' })
+loopd(colorPalette, (k, v) => tc('bg-' + k, { backgroundColor: v }));
 
-forEach(colorPalette, (k, v) => {
-  cases.push(['border-' + k, { borderColor: v }])
-  cases.push(['border-x-' + k, { borderLeftColor: v, borderRightColor: v }])
-  cases.push(['border-y-' + k, { borderTopColor: v, borderBottomColor: v }])
-  cases.push(['border-t-' + k, { borderTopColor: v }])
-  cases.push(['border-r-' + k, { borderRightColor: v }])
-  cases.push(['border-b-' + k, { borderBottomColor: v }])
-  cases.push(['border-l-' + k, { borderLeftColor: v }])
+tc('border', { borderWidth: 1 })
+tc('border-x', { borderLeftWidth: 1, borderRightWidth: 1 })
+tc('border-y', { borderTopWidth: 1, borderBottomWidth: 1 })
+tc('border-t', { borderTopWidth: 1 })
+tc('border-r', { borderRightWidth: 1 })
+tc('border-b', { borderBottomWidth: 1 })
+tc('border-l', { borderLeftWidth: 1 })
+
+loopd(colorPalette, (k, v) => {
+  tc('border-' + k, { borderColor: v })
+  tc('border-x-' + k, { borderLeftColor: v, borderRightColor: v })
+  tc('border-y-' + k, { borderTopColor: v, borderBottomColor: v })
+  tc('border-t-' + k, { borderTopColor: v })
+  tc('border-r-' + k, { borderRightColor: v })
+  tc('border-b-' + k, { borderBottomColor: v })
+  tc('border-l-' + k, { borderLeftColor: v })
 });
 
-[0, 2, 4, 8].forEach(v => {
-  cases.push(['border-' + v, { borderWidth: v }])
-  cases.push(['border-x-' + v, { borderLeftWidth: v, borderRightWidth: v }])
-  cases.push(['border-y-' + v, { borderTopWidth: v, borderBottomWidth: v }])
-  cases.push(['border-t-' + v, { borderTopWidth: v }])
-  cases.push(['border-r-' + v, { borderRightWidth: v }])
-  cases.push(['border-b-' + v, { borderBottomWidth: v }])
-  cases.push(['border-l-' + v, { borderLeftWidth: v }])
+loop([0, 2, 4, 8], v => {
+  tc('border-' + v, { borderWidth: v })
+  tc('border-x-' + v, { borderLeftWidth: v, borderRightWidth: v })
+  tc('border-y-' + v, { borderTopWidth: v, borderBottomWidth: v })
+  tc('border-t-' + v, { borderTopWidth: v })
+  tc('border-r-' + v, { borderRightWidth: v })
+  tc('border-b-' + v, { borderBottomWidth: v })
+  tc('border-l-' + v, { borderLeftWidth: v })
 });
 
-['solid', 'dashed', 'dotted', 'double', 'hidden', 'none'].forEach(v => {
-  cases.push(['border-' + v, { borderStyle: v }])
-});
+loop(['solid', 'dashed', 'dotted', 'double', 'hidden', 'none'], v => {
+  tc('border-' + v, { borderStyle: v })
+})
+
+tc('text-left', { textAlign: 'left' })
+tc('text-center', { textAlign: 'center' })
+tc('text-right', { textAlign: 'right' })
+tc('text-justify', { textAlign: 'justify' })
+tc('text-start', { textAlign: 'start' })
+tc('text-end', { textAlign: 'end' })
+
+tc('text-inherit', { color: 'inherit' })
+loopd(colorPalette, (k, v) => tc('text-' + k, { color: v }))
+
 
 cases.forEach(([spec, expected]) => it(spec, () => { compare(spec, expected) }))
