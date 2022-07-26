8e4e72db3dd7ed1c9ccf29bd49fcd75c2d3595f7
El P <prithvi.prabhu@gmail.com>
2022-07-26 12:59:12 -0700

    docs: Rework layout docs #67

--- docs/layout.md
+++ docs/layout.md
@@ -2,16 +2,16 @@
 
 Compose and arrange boxes to lay out content and control flow.
 
-## Basics
+## Basic
 
-By default each item passed to `view()` are laid out one below the other, with a `10px` gap.
+By default, items passed to `view()` are laid out one below the other.
 
 
 ```py
 view(
-    box(value='Top'),
-    box(value='Middle'),
-    box(value='Bottom'),
+    box('Top', style='border p-2'),
+    box('Middle', style='border p-2'),
+    box('Bottom', style='border p-2'),
 )
 ```
 
@@ -19,54 +19,80 @@ view(
 ![Screenshot](assets/screenshots/layout_basic.png)
 
 
-## Lay out horizontally
+## Horizontal layout
 
 Use `row()` to lay out multiple items horizontally, left to right.
 
-By default, items take up equal amounts of space, with a `10px` gap between the items.
-
 
 ```py
-view(row(
-    box(value='Left'),
-    box(value='Center'),
-    box(value='Right'),
-))
+view(
+    row(
+        box('Left', style='border p-2'),
+        box('Center', style='border p-2'),
+        box('Right', style='border p-2'),
+    )
+)
 ```
 
 
-`row(x, y, z)` is shorthand for `box(mode='row', items=[x, y, z])`.
+`row(x, y, z)` is shorthand for `box(mode='row', items=[x, y, z], style='flex flex-row gap-2')`.
 
 
 ```py
 view(box(
     mode='row',
     items=[
-        box(value='Left'),
-        box(value='Center'),
-        box(value='Right'),
+        box('Left', style='border p-2'),
+        box('Center', style='border p-2'),
+        box('Right', style='border p-2'),
     ]
 ))
-```
-
-
-Setting `mode='row'` produces the same result as wrapping items with `row()`.
-
-
-```py
-view(
-    box(value='Left'),
-    box(value='Center'),
-    box(value='Right'),
-    mode='row',
-)
 ```
 
 
 ![Screenshot](assets/screenshots/layout_row.png)
 
 
-## Lay out vertically
+## Adjust items to fit
+
+Set the `grow` style to expand items to fit.
+
+
+```py
+view(
+    row(
+        box('Left', style='border p-2 grow'),
+        box('Center', style='border p-2 grow'),
+        box('Right', style='border p-2 grow'),
+    )
+)
+```
+
+
+![Screenshot](assets/screenshots/layout_grow.png)
+
+
+## Adjust spacing
+
+Set a `gap` style to change the gap between items. The default gap is `gap-2`.
+
+
+```py
+view(
+    row(
+        box('Left', style='border p-2 grow'),
+        box('Center', style='border p-2 grow'),
+        box('Right', style='border p-2 grow'),
+        style='gap-8',
+    )
+)
+```
+
+
+![Screenshot](assets/screenshots/layout_gap.png)
+
+
+## Vertical layout
 
 Use `col()` to lay out multiple items vertically, top to bottom.
 
@@ -74,34 +100,35 @@ The example shows one row split into three columns containing three rows each.
 
 
 ```py
+style = 'border p-2'
 view(
     row(
         col(
-            box(value='North-west'),
-            box(value='West'),
-            box(value='South-west'),
+            box('North-west', style=style),
+            box('West', style=style),
+            box('South-west', style=style),
         ),
         col(
-            box(value='North'),
-            box(value='Center'),
-            box(value='South'),
+            box('North', style=style),
+            box('Center', style=style),
+            box('South', style=style),
         ),
         col(
-            box(value='North-east'),
-            box(value='East'),
-            box(value='South-east'),
+            box('North-east', style=style),
+            box('East', style=style),
+            box('South-east', style=style),
         ),
     ),
 )
 ```
 
 
-`col(x, y, z)` is shorthand for `box(mode='column', items=[x, y, z])`.
+`col(x, y, z)` is shorthand for `box(mode='col', items=[x, y, z], style='flex flex-col gap-2')`.
 
 
 ```py
 box(
-    mode='column',
+    mode='col',
     items=[
         box(value='North'),
         box(value='Center'),
@@ -114,7 +141,45 @@ box(
 ![Screenshot](assets/screenshots/layout_col.png)
 
 
-## Lay out in tabs
+## Combine horizontal and vertical layouts
+
+Combine `row()` and `col()` to create simple grid-like layouts
+
+The example shows one row split into three columns containing three rows each.
+
+
+```py
+style = 'border p-2'
+view(
+    row(
+        col(
+            box('North-west', style=style),
+            box('West', style=style),
+            box('South-west', style=style),
+            style='grow gap-2'
+        ),
+        col(
+            box('North', style=style),
+            box('Center', style=style),
+            box('South', style=style),
+            style='grow gap-2'
+        ),
+        col(
+            box('North-east', style=style),
+            box('East', style=style),
+            box('South-east', style=style),
+            style='grow gap-2'
+        ),
+        style='gap-2'
+    ),
+)
+```
+
+
+![Screenshot](assets/screenshots/layout_row_col.png)
+
+
+## Tabbed layout
 
 Set mode='tabs' on a box to lay out its items in tabs.
 
@@ -159,16 +224,16 @@ view(
 ![Screenshot](assets/screenshots/layout_tabs.png)
 
 
-## Lay out in tabs vertically
+## Accordion layout
 
-Set `layout='column'` to lay out tabs one below the other.
+Set `layout='col'` with `mode='tabs'` to lay out tabs one below the other, commonly known as an accordion.
 
 
 ```py
 view(
     box(
         mode='tabs',
-        layout='column',
+        layout='col',
         items=[
             box(
                 'Profile',
@@ -252,269 +317,21 @@ view(
 ![Screenshot](assets/screenshots/layout_tabs_icons.png)
 
 
-## Control scrolling
+## Advanced layouts
 
-Set `height=` or `width=` on a `row()` or `col()` to constrain size.
-Scrollbars automatically show up if their contents don't fit.
+For explicit control over layouts, use `box(items=[...])` instead of `row(...)` or `col(...)`.
 
-
-```py
-list1 = [box(lorem()) for _ in range(50)]
-list2 = [box(lorem()) for _ in range(50)]
-view(
-    "### Scroll together",
-    row(
-        col(*list1),
-        col(*list2),
-        height='500px',
-    ),
-    "### Scroll independently",
-    row(
-        col(*list1, height='500px'),
-        col(*list2, height='500px'),
-    )
-)
-```
-
-
-![Screenshot](assets/screenshots/layout_nest.png)
-
-
-## Control tiling
-
-Set `tile=` to control how items inside a view, row, or column are tiled along the main axis.
-
-- The main axis for a row is horizontal, starting at the left, and ending at the right.
-- The main axis for a column is vertical, starting at the top, and ending at the bottom
-
-`tile=` can be set to `start`, `center`, `end`, `between`, `around`, `evenly`, `stretch`, or `normal`.
-
-
-```py
-boxes = [box(text=f'{i + 1}', background='#666', width=100) for i in range(3)]
-row_style = dict(background='#eee')
-view(
-    "Start",
-    # Pack items from the start.
-    row(*boxes, tile='start', **row_style),
-
-    "Center",
-    # Pack items around the center.
-    row(*boxes, tile='center', **row_style),
-
-    "End",
-    # Pack items towards the end.
-    row(*boxes, tile='end', **row_style),
-
-    "Between",
-    # Distribute items evenly.
-    # The first item is flush with the start,
-    # the last is flush with the end.
-    row(*boxes, tile='between', **row_style),
-
-    "Around",
-    # Distribute items evenly.
-    # Items have a half-size space on either side.
-    row(*boxes, tile='around', **row_style),
-
-    "Evenly",
-    # Distribute items evenly.
-    # Items have equal space around them.
-    row(*boxes, tile='evenly', **row_style),
-
-    "Normal",
-    # Default alignment.
-    row(*boxes, tile='normal', **row_style),
-)
-```
-
-
-![Screenshot](assets/screenshots/layout_tile.png)
-
-
-## Control cross tiling
-
-Set `cross_tile=` to control how items inside a view, row, or column are tiled along the cross axis.
-
-- The cross axis for a row is vertical. starting at the top, and ending at the bottom
-- The cross axis for a column is horizontal, starting at the left, and ending at the right.
-
-`cross_tile=` can be set to `start`, `center`, `end`, `stretch`, or `normal`.
-
-
-```py
-boxes = [box(text=f'{i + 1}', background='#666', width=100) for i in range(3)]
-row_style = dict(height=150, background='#eee')
-view(
-    # Pack items from the start.
-    row(*boxes, cross_tile='start', **row_style),
-
-    # Pack items around the center.
-    row(*boxes, cross_tile='center', **row_style),
-
-    # Pack items towards the end.
-    row(*boxes, cross_tile='end', **row_style),
-
-    # Stretch items to fit.
-    row(*boxes, cross_tile='stretch', **row_style),
-
-    # Default alignment.
-    row(*boxes, cross_tile='normal', **row_style),
-)
-```
-
-
-![Screenshot](assets/screenshots/layout_cross_tile.png)
-
-
-## Control spacing
-
-Set `gap=` to control the spacing between items. The default gap is `10` or `'10px'`.
-
-
-```py
-view(
-    box(value='Top'),
-    box(value='Middle'),
-    box(value='Bottom'),
-    gap=25,
-)
-```
-
-
-![Screenshot](assets/screenshots/layout_gap.png)
-
-
-## Control wrapping
-
-Set `wrap=` to control how items are wrapped inside a view, row, or column.
-
-`wrap=` can be set to `start`, `center`, `end`, `between`, `around`, `evenly`, `stretch`, or `normal`.
-
-
-```py
-boxes = [box(text=f'{i + 1}', background='#666', width=150, height=50) for i in range(9)]
-row_style = dict(height=300, background='#eee')
-view(
-    # Pack items from the start.
-    row(*boxes, wrap='start', **row_style),
-
-    # Pack items around the center.
-    row(*boxes, wrap='center', **row_style),
-
-    # Pack items towards the end.
-    row(*boxes, wrap='end', **row_style),
-
-    # Distribute items evenly.
-    # The first item is flush with the start,
-    # the last is flush with the end.
-    row(*boxes, wrap='between', **row_style),
-
-    # Distribute items evenly.
-    # Items have a half-size space on either side.
-    row(*boxes, wrap='around', **row_style),
-
-    # Distribute items evenly.
-    # Items have equal space around them.
-    row(*boxes, wrap='evenly', **row_style),
-
-    # Default alignment.
-    row(*boxes, wrap='normal', **row_style),
-)
-```
-
-
-![Screenshot](assets/screenshots/layout_wrap.png)
-
-
-## Grow or shrink some items
-
-Set `grow=` or `shrink=` to specify what amount of the available space the item should take up
-inside a view, row, or column.
-
-Setting `grow=` expands the item. Setting `shrink=` contracts the item. Both are proportions.
-
-By default, items are grown or shrunk based on their initial size. To resize them on a different basis,
-set `basis=` to the value you want.
-
-- `basis=0` means "distribute available space assuming that the initial size is zero".
-- `basis='20px'` means "distribute available space assuming that the initial size is 20px".
-- The default behavior (if `basis=` is not set) is to assume that the initial size is the size of the item's content.
-
-
-```py
-box_style = dict(background='#666')
-row_style = dict(background='#eee')
-view(
-    '1:?:?',
-    row(
-        # Take up all available space.
-        box('a', grow=1, **box_style),
-        box('b', width=50, **box_style),
-        box('c', width=50, **box_style),
-        **row_style,
-    ),
-    '1:1:?',
-    row(
-        # Take up one part of available space = 1 / (1 + 1).
-        box('a', grow=1, **box_style),
-        # Take up one part of available space = 1 / (1 + 1).
-        box('b', grow=1, **box_style),
-        box('c', width=50, **box_style),
-        **row_style,
-    ),
-    '2:1:?',
-    row(
-        # Take up two parts of available space = 2 / (2 + 1).
-        box('a', grow=2, **box_style),
-        # Take up one part of available space = 1 / (2 + 1).
-        box('b', grow=1, **box_style),
-        box('c', width=50, **box_style),
-        **row_style,
-    ),
-    '1:2:3:?',
-    row(
-        # Take up one part of available space = 1 / (1 + 2 + 3).
-        box('a', grow=1, **box_style),
-        # Take up two parts of available space = 2 / (1 + 2 + 3).
-        box('b', grow=2, **box_style),
-        # Take up three parts of available space = 3 / (1 + 2 + 3).
-        box('c', grow=3, **box_style),
-        box('d', width=50, **box_style),
-        **row_style,
-    ),
-    '1:1:1:1',
-    row(
-        # Divide available space equally.
-        box('a', grow=1, **box_style),
-        box('b', grow=1, **box_style),
-        box('c', grow=1, **box_style),
-        box('d', grow=1, **box_style),
-        **row_style,
-    ),
-)
-```
-
-
-![Screenshot](assets/screenshots/layout_grow_shrink.png)
-
-
-## Center content vertically
-
-Use `tile='center'` to center content vertically inside a box.
-
-The following example centers content both horizontally and vertically.
+The example below creates a 4-column grid layout using `style='grid'`.
 
 
 ```py
 view(
     box(
-        '# Donuts',
-        tile='center', cross_tile='center',
-        height='300px', background='$foreground', color='$background',
+        items=[box(f'Box {i}', style='p-2 border') for i in range(1, 13)],
+        style='grid grid-cols-4 gap-2',
     )
 )
 ```
 
 
-![Screenshot](assets/screenshots/layout_vertical_alignment.png)
+![Screenshot](assets/screenshots/layout_grid.png)
--- py/pkg/docs/layout.py
+++ py/pkg/docs/layout.py
@@ -18,80 +18,96 @@ from h2o_nitro import View, box, row, col, option, lorem
 # # Layout
 # Compose and arrange boxes to lay out content and control flow.
 
-# ## Basics
-# By default each item passed to `view()` are laid out one below the other, with a `10px` gap.
+# ## Basic
+# By default, items passed to `view()` are laid out one below the other.
 def layout_basic(view: View):  # height 3
     view(
-        box(value='Top'),
-        box(value='Middle'),
-        box(value='Bottom'),
+        box('Top', style='border p-2'),
+        box('Middle', style='border p-2'),
+        box('Bottom', style='border p-2'),
     )
 
 
-# ## Lay out horizontally
+# ## Horizontal layout
 # Use `row()` to lay out multiple items horizontally, left to right.
-#
-# By default, items take up equal amounts of space, with a `10px` gap between the items.
 def layout_row(view: View):  # height 2
-    view(row(
-        box(value='Left'),
-        box(value='Center'),
-        box(value='Right'),
-    ))
+    view(
+        row(
+            box('Left', style='border p-2'),
+            box('Center', style='border p-2'),
+            box('Right', style='border p-2'),
+        )
+    )
 
 
-# `row(x, y, z)` is shorthand for `box(mode='row', items=[x, y, z])`.
+# `row(x, y, z)` is shorthand for `box(mode='row', items=[x, y, z], style='flex flex-row gap-2')`.
 def layout_row_alt(view: View):
     view(box(
         mode='row',
         items=[
-            box(value='Left'),
-            box(value='Center'),
-            box(value='Right'),
+            box('Left', style='border p-2'),
+            box('Center', style='border p-2'),
+            box('Right', style='border p-2'),
         ]
     ))
 
 
-# Setting `mode='row'` produces the same result as wrapping items with `row()`.
-def layout_row_alt2(view: View):
+# ## Adjust items to fit
+# Set the `grow` style to expand items to fit.
+def layout_grow(view: View):  # height 2
     view(
-        box(value='Left'),
-        box(value='Center'),
-        box(value='Right'),
-        mode='row',
+        row(
+            box('Left', style='border p-2 grow'),
+            box('Center', style='border p-2 grow'),
+            box('Right', style='border p-2 grow'),
+        )
     )
 
 
-# ## Lay out vertically
+# ## Adjust spacing
+# Set a `gap` style to change the gap between items. The default gap is `gap-2`.
+def layout_gap(view: View):  # height 2
+    view(
+        row(
+            box('Left', style='border p-2 grow'),
+            box('Center', style='border p-2 grow'),
+            box('Right', style='border p-2 grow'),
+            style='gap-8',
+        )
+    )
+
+
+# ## Vertical layout
 # Use `col()` to lay out multiple items vertically, top to bottom.
 #
 # The example shows one row split into three columns containing three rows each.
 def layout_col(view: View):  # height 3
+    style = 'border p-2'
     view(
         row(
             col(
-                box(value='North-west'),
-                box(value='West'),
-                box(value='South-west'),
+                box('North-west', style=style),
+                box('West', style=style),
+                box('South-west', style=style),
             ),
             col(
-                box(value='North'),
-                box(value='Center'),
-                box(value='South'),
+                box('North', style=style),
+                box('Center', style=style),
+                box('South', style=style),
             ),
             col(
-                box(value='North-east'),
-                box(value='East'),
-                box(value='South-east'),
+                box('North-east', style=style),
+                box('East', style=style),
+                box('South-east', style=style),
             ),
         ),
     )
 
 
-# `col(x, y, z)` is shorthand for `box(mode='column', items=[x, y, z])`.
+# `col(x, y, z)` is shorthand for `box(mode='col', items=[x, y, z], style='flex flex-col gap-2')`.
 def layout_col_alt2(view: View):
     box(
-        mode='column',
+        mode='col',
         items=[
             box(value='North'),
             box(value='Center'),
@@ -100,7 +116,38 @@ def layout_col_alt2(view: View):
     )
 
 
-# ## Lay out in tabs
+# ## Combine horizontal and vertical layouts
+# Combine `row()` and `col()` to create simple grid-like layouts
+#
+# The example shows one row split into three columns containing three rows each.
+def layout_row_col(view: View):  # height 3
+    style = 'border p-2'
+    view(
+        row(
+            col(
+                box('North-west', style=style),
+                box('West', style=style),
+                box('South-west', style=style),
+                style='grow gap-2'
+            ),
+            col(
+                box('North', style=style),
+                box('Center', style=style),
+                box('South', style=style),
+                style='grow gap-2'
+            ),
+            col(
+                box('North-east', style=style),
+                box('East', style=style),
+                box('South-east', style=style),
+                style='grow gap-2'
+            ),
+            style='gap-2'
+        ),
+    )
+
+
+# ## Tabbed layout
 # Set mode='tabs' on a box to lay out its items in tabs.
 #
 # The `text` of each child item is used as the tab's label.
@@ -138,13 +185,13 @@ def layout_tabs(view: View):  # height 5
     )
 
 
-# ## Lay out in tabs vertically
-# Set `layout='column'` to lay out tabs one below the other.
+# ## Accordion layout
+# Set `layout='col'` with `mode='tabs'` to lay out tabs one below the other, commonly known as an accordion.
 def layout_tabs_vertical(view: View):  # height 5
     view(
         box(
             mode='tabs',
-            layout='column',
+            layout='col',
             items=[
                 box(
                     'Profile',
@@ -217,220 +264,14 @@ def layout_tabs_icons(view: View):  # height 5
     )
 
 
-# ## Control scrolling
-# Set `height=` or `width=` on a `row()` or `col()` to constrain size.
-# Scrollbars automatically show up if their contents don't fit.
-def layout_nest(view: View):  # height 13
-    list1 = [box(lorem()) for _ in range(50)]
-    list2 = [box(lorem()) for _ in range(50)]
-    view(
-        "### Scroll together",
-        row(
-            col(*list1),
-            col(*list2),
-            height='500px',
-        ),
-        "### Scroll independently",
-        row(
-            col(*list1, height='500px'),
-            col(*list2, height='500px'),
-        )
-    )
-
-
-# ## Control tiling
-# Set `tile=` to control how items inside a view, row, or column are tiled along the main axis.
+# ## Advanced layouts
+# For explicit control over layouts, use `box(items=[...])` instead of `row(...)` or `col(...)`.
 #
-# - The main axis for a row is horizontal, starting at the left, and ending at the right.
-# - The main axis for a column is vertical, starting at the top, and ending at the bottom
-#
-# `tile=` can be set to `start`, `center`, `end`, `between`, `around`, `evenly`, `stretch`, or `normal`.
-def layout_tile(view: View):  # height 9
-    boxes = [box(text=f'{i + 1}', background='#666', width=100) for i in range(3)]
-    row_style = dict(background='#eee')
-    view(
-        "Start",
-        # Pack items from the start.
-        row(*boxes, tile='start', **row_style),
-
-        "Center",
-        # Pack items around the center.
-        row(*boxes, tile='center', **row_style),
-
-        "End",
-        # Pack items towards the end.
-        row(*boxes, tile='end', **row_style),
-
-        "Between",
-        # Distribute items evenly.
-        # The first item is flush with the start,
-        # the last is flush with the end.
-        row(*boxes, tile='between', **row_style),
-
-        "Around",
-        # Distribute items evenly.
-        # Items have a half-size space on either side.
-        row(*boxes, tile='around', **row_style),
-
-        "Evenly",
-        # Distribute items evenly.
-        # Items have equal space around them.
-        row(*boxes, tile='evenly', **row_style),
-
-        "Normal",
-        # Default alignment.
-        row(*boxes, tile='normal', **row_style),
-    )
-
-
-# ## Control cross tiling
-# Set `cross_tile=` to control how items inside a view, row, or column are tiled along the cross axis.
-#
-# - The cross axis for a row is vertical. starting at the top, and ending at the bottom
-# - The cross axis for a column is horizontal, starting at the left, and ending at the right.
-#
-# `cross_tile=` can be set to `start`, `center`, `end`, `stretch`, or `normal`.
-def layout_cross_tile(view: View):  # height 9
-    boxes = [box(text=f'{i + 1}', background='#666', width=100) for i in range(3)]
-    row_style = dict(height=150, background='#eee')
-    view(
-        # Pack items from the start.
-        row(*boxes, cross_tile='start', **row_style),
-
-        # Pack items around the center.
-        row(*boxes, cross_tile='center', **row_style),
-
-        # Pack items towards the end.
-        row(*boxes, cross_tile='end', **row_style),
-
-        # Stretch items to fit.
-        row(*boxes, cross_tile='stretch', **row_style),
-
-        # Default alignment.
-        row(*boxes, cross_tile='normal', **row_style),
-    )
-
-
-# ## Control spacing
-# Set `gap=` to control the spacing between items. The default gap is `10` or `'10px'`.
-def layout_gap(view: View):  # height 3
-    view(
-        box(value='Top'),
-        box(value='Middle'),
-        box(value='Bottom'),
-        gap=25,
-    )
-
-
-# ## Control wrapping
-# Set `wrap=` to control how items are wrapped inside a view, row, or column.
-#
-# `wrap=` can be set to `start`, `center`, `end`, `between`, `around`, `evenly`, `stretch`, or `normal`.
-def layout_wrap(view: View):  # height 23
-    boxes = [box(text=f'{i + 1}', background='#666', width=150, height=50) for i in range(9)]
-    row_style = dict(height=300, background='#eee')
-    view(
-        # Pack items from the start.
-        row(*boxes, wrap='start', **row_style),
-
-        # Pack items around the center.
-        row(*boxes, wrap='center', **row_style),
-
-        # Pack items towards the end.
-        row(*boxes, wrap='end', **row_style),
-
-        # Distribute items evenly.
-        # The first item is flush with the start,
-        # the last is flush with the end.
-        row(*boxes, wrap='between', **row_style),
-
-        # Distribute items evenly.
-        # Items have a half-size space on either side.
-        row(*boxes, wrap='around', **row_style),
-
-        # Distribute items evenly.
-        # Items have equal space around them.
-        row(*boxes, wrap='evenly', **row_style),
-
-        # Default alignment.
-        row(*boxes, wrap='normal', **row_style),
-    )
-
-
-# ## Grow or shrink some items
-# Set `grow=` or `shrink=` to specify what amount of the available space the item should take up
-# inside a view, row, or column.
-#
-# Setting `grow=` expands the item. Setting `shrink=` contracts the item. Both are proportions.
-#
-# By default, items are grown or shrunk based on their initial size. To resize them on a different basis,
-# set `basis=` to the value you want.
-#
-# - `basis=0` means "distribute available space assuming that the initial size is zero".
-# - `basis='20px'` means "distribute available space assuming that the initial size is 20px".
-# - The default behavior (if `basis=` is not set) is to assume that the initial size is the size of the item's content.
-def layout_grow_shrink(view: View):  # height 7
-    box_style = dict(background='#666')
-    row_style = dict(background='#eee')
-    view(
-        '1:?:?',
-        row(
-            # Take up all available space.
-            box('a', grow=1, **box_style),
-            box('b', width=50, **box_style),
-            box('c', width=50, **box_style),
-            **row_style,
-        ),
-        '1:1:?',
-        row(
-            # Take up one part of available space = 1 / (1 + 1).
-            box('a', grow=1, **box_style),
-            # Take up one part of available space = 1 / (1 + 1).
-            box('b', grow=1, **box_style),
-            box('c', width=50, **box_style),
-            **row_style,
-        ),
-        '2:1:?',
-        row(
-            # Take up two parts of available space = 2 / (2 + 1).
-            box('a', grow=2, **box_style),
-            # Take up one part of available space = 1 / (2 + 1).
-            box('b', grow=1, **box_style),
-            box('c', width=50, **box_style),
-            **row_style,
-        ),
-        '1:2:3:?',
-        row(
-            # Take up one part of available space = 1 / (1 + 2 + 3).
-            box('a', grow=1, **box_style),
-            # Take up two parts of available space = 2 / (1 + 2 + 3).
-            box('b', grow=2, **box_style),
-            # Take up three parts of available space = 3 / (1 + 2 + 3).
-            box('c', grow=3, **box_style),
-            box('d', width=50, **box_style),
-            **row_style,
-        ),
-        '1:1:1:1',
-        row(
-            # Divide available space equally.
-            box('a', grow=1, **box_style),
-            box('b', grow=1, **box_style),
-            box('c', grow=1, **box_style),
-            box('d', grow=1, **box_style),
-            **row_style,
-        ),
-    )
-
-
-# ## Center content vertically
-# Use `tile='center'` to center content vertically inside a box.
-#
-# The following example centers content both horizontally and vertically.
-def layout_vertical_alignment(view: View):  # height 5
+# The example below creates a 4-column grid layout using `style='grid'`.
+def layout_grid(view: View):  # height 5
     view(
         box(
-            '# Donuts',
-            tile='center', cross_tile='center',
-            height='300px', background='$foreground', color='$background',
+            items=[box(f'Box {i}', style='p-2 border') for i in range(1, 13)],
+            style='grid grid-cols-4 gap-2',
         )
     )
