2f13816cb6e7bb104400475279a54785c9f461a8
El P <prithvi.prabhu@gmail.com>
2022-08-05 11:50:45 -0700

    docs: Remove obsolete example #67

--- py/pkg/docs/styling.py
+++ py/pkg/docs/styling.py
@@ -18,17 +18,6 @@ from h2o_nitro import View, box, row, col, option, lorem
 # # Styling
 # Change how boxes look: colors, borders, sizing, margins, and padding.
 
-# ## Set background color
-# Set `background=` to apply a background color.
-#
-# The text color is automatically changed to a contrasting color if not specified.
-# A padding is automatically applied if not specified.
-def styling_background(view: View):
     text = lorem(3)
     view(
-        box(text, background='#e63946'),
-        box(text, background='#f1faee'),
-        box(text, background='#a8dadc'),
-        box(text, background='#457b9d'),
-        box(text, background='#1d3557'),
     )
