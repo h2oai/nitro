87b07727dc9c6a7093c471b48c87ee851884c48f
elp <prithvi.prabhu@gmail.com>
2022-08-09 12:47:46 -0700

    docs: Improve cards examples #67

--- py/pkg/docs/cards.py
+++ py/pkg/docs/cards.py
@@ -19,111 +19,78 @@ from h2o_nitro import View, box, row, col, option, lorem
 # Create content cards by stacking boxes together.
 
 # ## Basic
-def card_basic(view: View):
+def card_basic(view: View):  # height 4
     icon = '''
     <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
     </svg>
     '''
-
-    def card(title, caption, score):
-        return box(
-            box(score,
+    view(
+        box(
+            box('4.2',
                 style='absolute top-4 right-4 rounded-full px-3 py-1.5 bg-green-100 text-green-600 font-medium text-xs'),
             col(
                 box(icon, mode='svg', style='w-10 h-10'),
-                box(title, style='text-xl font-bold text-gray-900'),
-                box(caption, style='text-sm'),
+                box(lorem(), style='text-xl font-bold text-gray-900'),
+                box(lorem(2), style='text-sm'),
                 style='text-gray-500 pr-8',
             ),
             style='relative p-8 border shadow hover:shadow-xl rounded-xl',
-        )
-
-    view(
-        box(
-            card(lorem(), lorem(2), '4.2'),
-            card(lorem(), lorem(2), '2.6'),
-            card(lorem(), lorem(2), '3.9'),
-            card(lorem(), lorem(2), '5.0'),
-            style='grid grid-cols-2 grid-rows-2 gap-4',
-        )
+        ),
+        style='w-96',
     )
 
 
 # ## With gradient border
-def card_gradient_border(view: View):
-    def card(title, caption):
-        return box(
+def card_gradient_border(view: View):  # height 4
+    view(
+        box(
             box(
                 box(
-                    box(title, style='text-xl font-bold text-gray-900'),
-                    box(caption, style='mt-2 text-sm text-gray-500'),
+                    box('To space and beyond', style='text-xl font-bold text-gray-900'),
+                    box(lorem(2), style='mt-2 text-sm text-gray-500'),
                     style='mt-16 pr-8',
                 ),
                 style='absolute inset-1 bg-white p-8 rounded-xl'
             ),
             style='relative h-72 shadow-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl',
-        )
-
-    view(
-        box(
-            card(lorem(), lorem(2)),
-            card(lorem(), lorem(2)),
-            card(lorem(), lorem(2)),
-            card(lorem(), lorem(2)),
-            style='grid grid-cols-2 grid-rows-2 gap-4',
-        )
+        ),
+        style='w-96',
     )
 
 
 # ## With image
 def card_image(view: View):
-    def card(title, caption, image):
-        return box(
-            box(image=image, style='object-cover w-full h-56'),
+    view(
+        box(
+            box(image='sample.jpg', style='object-cover w-full h-56'),
             box(
-                box(title, style='text-xl font-bold'),
-                box(caption, style='mt-2 text-sm text-gray-500'),
+                box('To space and beyond', style='text-xl font-bold'),
+                box(lorem(2), style='mt-2 text-sm text-gray-500'),
                 box('Find out more →',
                     style='inline-block pb-1 mt-4 font-medium text-indigo-600 border-b border-indigo-500'),
                 style='p-6',
             ),
             style='overflow-hidden border border-gray-100 rounded-lg shadow-sm',
-        )
-
-    view(
-        box(
-            card(lorem(), lorem(2), 'sample.jpg'),
-            card(lorem(), lorem(2), 'sample.jpg'),
-            card(lorem(), lorem(2), 'sample.jpg'),
-            card(lorem(), lorem(2), 'sample.jpg'),
-            style='grid grid-cols-2 grid-rows-2 gap-4',
-        )
+        ),
+        style='w-96',
     )
 
 
 # ## Dark with image
 def card_dark_image(view: View):
-    def card(category, title, caption, image):
-        return box(
-            box(image=image, style='object-cover w-full h-56'),
+    view(
+        box(
+            box(image='sample.jpg', style='object-cover w-full h-56'),
             box(
-                box(category, style='text-xs text-gray-400'),
-                box(title, style='text-sm text-white'),
-                box(caption, style='mt-1 text-xs text-gray-300'),
+                box('Rocketry', style='text-xs text-gray-400'),
+                box('To space and beyond', style='text-sm text-white'),
+                box(lorem(2), style='mt-1 text-xs text-gray-300'),
                 style='p-4 bg-gray-900',
             ),
             style='overflow-hidden rounded-2xl',
-        )
-
-    view(
-        box(
-            card('Travel', lorem(), lorem(2), 'sample.jpg'),
-            card('Rocketry', lorem(), lorem(2), 'sample.jpg'),
-            card('Space', lorem(), lorem(2), 'sample.jpg'),
-            card('Food', lorem(), lorem(2), 'sample.jpg'),
-            style='grid grid-cols-2 grid-rows-2 gap-4',
-        )
+        ),
+        style='w-96',
     )
 
 
@@ -142,7 +109,7 @@ def card_stats(view: View):
                 ),
                 style='justify-between',
             ),
-            box(lorem(7), style='mt-4 pr-12 text-sm text-gray-500'),
+            box(lorem(4), style='mt-4 pr-12 text-sm text-gray-500'),
             row(
                 box(
                     box('2nd Apr 2024', style='text-xs text-gray-500'),
@@ -156,4 +123,5 @@ def card_stats(view: View):
             box(style='absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600'),
             style='relative p-8 overflow-hidden border border-gray-100 rounded-lg',
         ),
+        style='w-96',
     )
