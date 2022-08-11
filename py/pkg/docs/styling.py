d94510aa7a819be842c1fcf0572b23d520b03358
elp <prithvi.prabhu@gmail.com>
2022-08-10 17:15:01 -0700

    docs: Improve card styling example #67

--- py/pkg/docs/styling.py
+++ py/pkg/docs/styling.py
@@ -81,18 +81,18 @@ def styling_alert(view: View):  # height 2
     view(box(text, style='font-medium text-sm text-red-700 bg-red-50 p-4 border-l-4 border-red-700'), ['Restart'])
 
 
-# ## Card
+# ## Card example
 # The following example starts with some nested and incrementally applies styles to create a card.
 def styling_card(view: View):  # height 7
     title = 'Space Travel'
     text = lorem(2)
     image = box(image='sample.jpg')
-    view(box(image, box(title), text), ['Resize Card'])
-    view(box(image, col(box(title), text), style='w-72'), ['Round corners'])
-    view(box(image, col(box(title), text), style='w-72 rounded-xl'), ['Crop contents'])
-    view(box(image, col(box(title), text), style='w-72 rounded-xl overflow-hidden'), ['Add shadow'])
-    view(box(image, col(box(title), text), style='w-72 rounded-xl overflow-hidden shadow-xl'), ['Add padding'])
-    view(box(image, col(box(title), text, style='p-4'), style='w-72 rounded-xl overflow-hidden shadow-xl'), ['Make title bold'])
+    view(box(image, col(title, text)), ['Resize Card'])
+    view(box(image, col(title, text), style='w-72'), ['Round corners'])
+    view(box(image, col(title, text), style='w-72 rounded-xl'), ['Crop contents'])
+    view(box(image, col(title, text), style='w-72 rounded-xl overflow-hidden'), ['Add shadow'])
+    view(box(image, col(title, text), style='w-72 rounded-xl overflow-hidden shadow-xl'), ['Add padding'])
+    view(box(image, col(title, text, style='p-4'), style='w-72 rounded-xl overflow-hidden shadow-xl'), ['Make title bold'])
     view(box(image, col(box(title, style='font-bold'), text, style='p-4'), style='w-72 rounded-xl overflow-hidden shadow-xl'), ['Make title larger'])
     view(box(image, col(box(title, style='font-bold text-xl'), text, style='p-4'), style='w-72 rounded-xl overflow-hidden shadow-xl'), ['Make text smaller'])
     view(box(image, col(box(title, style='font-bold text-xl'), box(text, style='text-sm'), style='p-4'), style='w-72 rounded-xl overflow-hidden shadow-xl'), ['Restart'])
