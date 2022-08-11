c4748f668633c3edb643e6499636e05afd754bb1
elp <prithvi.prabhu@gmail.com>
2022-08-10 17:16:58 -0700

    docs: Add tabbed form example #67

--- py/pkg/docs/forms.py
+++ py/pkg/docs/forms.py
@@ -93,3 +93,31 @@ def form_improved(view: View):  # height 5
 
     Sign up: {action}
     ''')
+
+
+# ## Tabs
+# Groups together related boxes inside `row()` to display a tabbed layout.
+def form_tabs(view: View):  # height 4
+    view(
+        row(
+            box(
+                box('First name', value='Boaty'),
+                box('Last name', value='McBoatface'),
+                box('Age', value=42),
+                title='Profile',
+            ),
+            box(
+                box('Billing address line 1', value=''),
+                box('Billing address line 2', value=''),
+                row(box('City', value=''), box('State', value=''), box('Zip', value='')),
+                title='Billing Address',
+            ),
+            box(
+                box('Shipping address line 1', value=''),
+                box('Shipping address line 2', value=''),
+                row(box('City', value=''), box('State', value=''), box('Zip', value='')),
+                title='Shipping Address',
+            ),
+        )
+    )
+
