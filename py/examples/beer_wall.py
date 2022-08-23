262ae54a2f7d5400c9af2ea46654021c7a33e5e4
elp <prithvi.prabhu@gmail.com>
2022-08-23 11:47:31 -0700

    app: Beer wall, interactive

---
+++ py/examples/beer_wall.py
@@ -0,0 +1,60 @@
+# ===
+# About: 99 bottles of beer
+# Author: Prithvi Prabhu <prithvi.prabhu@gmail.com>
+# License: Apache-2.0
+# Source: https://github.com/h2oai/nitro/py/examples
+# Keywords: [basic, flask]
+#
+# Setup:
+# FILE requirements.txt EOF
+# flask
+# simple-websocket
+# h2o-nitro[web]
+# EOF
+# RUN python -m pip install -r requirements.txt
+# ENV FLASK_APP __file__
+# START python -m flask --debug run
+# ===
+
+from h2o_nitro import View
+
+
+def main(view: View):
+    for i in range(99, 0, -1):  # Decrement from 99 to 1
+        view(f"""
+        ## {i} bottles of beer
+        
+        {i} bottles of beer on the wall, {i} bottles of beer.
+
+        Take one down, pass it around, {i - 1} bottles of beer on the wall...
+        """)
+
+
+nitro = View(main, title='99 bottles of beer', caption='v1.0')
+
+# ┌─────────────── Flask Boilerplate ───────────────┐
+
+import simple_websocket
+from flask import Flask, request, send_from_directory
+from h2o_nitro_web import web_directory
+
+app = Flask(__name__, static_folder=web_directory, static_url_path='')
+
+
+@app.route('/')
+def home_page():
+    return send_from_directory(web_directory, 'index.html')
+
+
+@app.route('/nitro', websocket=True)
+def socket():
+    ws = simple_websocket.Server(request.environ)
+    try:
+        nitro.serve(ws.send, ws.receive)
+    except simple_websocket.ConnectionClosed:
+        pass
+    return ''
+
+
+if __name__ == '__main__':
+    app.run()
