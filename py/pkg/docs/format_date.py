f03ea33c60539c458a2e4054f02cc733d3d74962
elp <prithvi.prabhu@gmail.com>
2022-09-07 10:47:49 -0700

    docs: Add date/time length formatting docs #20

--- py/pkg/docs/format_date.py
+++ py/pkg/docs/format_date.py
@@ -66,3 +66,43 @@ def format_date_datetime(view: View):  # height 4
         box('=UK: {now date time}', data=dict(now=now), locale='en-GB'),
     )
 
+
+# ## Date style
+# Add one of `date-xl` (extra long), `date-l` (long), `date-m` (medium), or `date-s` (short) styles
+# for additional control.
+def format_date_length(view: View):  # height 6
+    now = datetime.datetime.now().astimezone().isoformat()
+    view(
+        box('=ISO: {now}', data=dict(now=now)),  # Raw date string (not formatted)
+        '## US',
+        box('=Full: {now date-xl}', data=dict(now=now), locale='en-US'),
+        box('=Long: {now date-l}', data=dict(now=now), locale='en-US'),
+        box('=Medium: {now date-m}', data=dict(now=now), locale='en-US'),
+        box('=Short: {now date-s}', data=dict(now=now), locale='en-US'),
+        '## UK',
+        box('=Full: {now date-xl}', data=dict(now=now), locale='en-GB'),
+        box('=Long: {now date-l}', data=dict(now=now), locale='en-GB'),
+        box('=Medium: {now date-m}', data=dict(now=now), locale='en-GB'),
+        box('=Short: {now date-s}', data=dict(now=now), locale='en-GB'),
+    )
+
+
+# ## Time style
+# Add one of `time-xl` (extra long), `time-l` (long), `time-m` (medium), or `time-s` (short) styles
+# for additional control.
+def format_date_time_length(view: View):  # height 6
+    now = datetime.datetime.now().astimezone().isoformat()
+    view(
+        box('=ISO: {now}', data=dict(now=now)),  # Raw date string (not formatted)
+        '## US',
+        box('=Full: {now time-xl}', data=dict(now=now), locale='en-US'),
+        box('=Long: {now time-l}', data=dict(now=now), locale='en-US'),
+        box('=Medium: {now time-m}', data=dict(now=now), locale='en-US'),
+        box('=Short: {now time-s}', data=dict(now=now), locale='en-US'),
+        '## UK',
+        box('=Full: {now time-xl}', data=dict(now=now), locale='en-GB'),
+        box('=Long: {now time-l}', data=dict(now=now), locale='en-GB'),
+        box('=Medium: {now time-m}', data=dict(now=now), locale='en-GB'),
+        box('=Short: {now time-s}', data=dict(now=now), locale='en-GB'),
+    )
+
