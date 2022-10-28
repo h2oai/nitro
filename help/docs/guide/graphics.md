b0b66ae07ad11cab00373d75675999f566384f09
elp <prithvi.prabhu@gmail.com>
2022-10-27 17:33:04 -0700

    docs: Add docs for all graphic primitives #88

--- help/docs/guide/graphics.md
+++ help/docs/guide/graphics.md
@@ -7,19 +7,59 @@ Draw microcharts, sparklines, and other data graphics.
 
 ## Line Y
 
+Set `mode='g line-y'` to draw line and area charts.
+
+For a single curve, set `data=` to a sequence of normalized y-coordinates.
+
+For dual curves, set `data=` to a sequence of normalized `[low, high]` y-coordinates.
+
+
 ```py
-view(
-    box(
-        mode='g line-y',
-        style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
-        data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 6,
+view(row(
+    # Single curve:
+    col(
+        # Stroke and fill:
+        box(
+            mode='g line-y',
+            style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
+            data=[0.5, 0.65, 0.5, 0.4, 0.95, 0.05, 0.5, 0.5, 0.6, 0.5, 0.5] * 6,
+        ),
+        # Stroke only:
+        box(
+            mode='g line-y',
+            style='w-48 h-8 fill-none stroke-indigo-700',
+            data=[0.5, 0.65, 0.5, 0.4, 0.95, 0.05, 0.5, 0.5, 0.6, 0.5, 0.5] * 6,
+        ),
+        # Fill only:
+        box(
+            mode='g line-y',
+            style='w-48 h-8 fill-indigo-700 stroke-none',
+            data=[0.5, 0.65, 0.5, 0.4, 0.95, 0.05, 0.5, 0.5, 0.6, 0.5, 0.5] * 6,
+        ),
     ),
-    box(
-        mode='g line-y',
-        style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
-        data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+    # Dual curve:
+    col(
+        # Stroke and fill:
+        box(
+            mode='g line-y',
+            style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
+            data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+        ),
+
+        # Stroke only:
+        box(
+            mode='g line-y',
+            style='w-48 h-8 fill-none stroke-indigo-700',
+            data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+        ),
+        # Fill only:
+        box(
+            mode='g line-y',
+            style='w-48 h-8 fill-indigo-700 stroke-none',
+            data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+        ),
     ),
-)
+))
 ```
 
 
@@ -28,19 +68,59 @@ view(
 
 ## Step Y
 
+Set `mode='g step-y'` to draw step charts. Step charts are similar to line charts, except that adjacent points are
+connected using discrete steps instead of line segments.
+
+For a single curve, set `data=` to a sequence of normalized y-coordinates.
+
+For dual curves, set `data=` to a sequence of normalized `[low, high]` y-coordinates.
+
+
 ```py
-view(
-    box(
-        mode='g step-y',
-        style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
-        data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 6,
+view(row(
+    # Single curve:
+    col(
+        # Stroke and fill:
+        box(
+            mode='g step-y',
+            style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
+            data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 6,
+        ),
+        # Stroke only:
+        box(
+            mode='g step-y',
+            style='w-48 h-8 fill-none stroke-indigo-700',
+            data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 6,
+        ),
+        # Fill only:
+        box(
+            mode='g step-y',
+            style='w-48 h-8 fill-indigo-700 stroke-none',
+            data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 6,
+        ),
     ),
-    box(
-        mode='g step-y',
-        style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
-        data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+    # Dual curve:
+    col(
+        # Stroke and fill:
+        box(
+            mode='g step-y',
+            style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
+            data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
+        ),
+        # Stroke only:
+        box(
+            mode='g step-y',
+            style='w-48 h-8 fill-none stroke-indigo-700',
+            data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
+        ),
+        # Fill only:
+        box(
+            mode='g step-y',
+            style='w-48 h-8 fill-indigo-700 stroke-none',
+            data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
+        ),
     ),
-)
+))
 ```
 
 
@@ -49,17 +129,26 @@ view(
 
 ## Interval Y
 
+Set `mode='g interval-y'` to draw bar/column charts.
+
+For simple bars, set `data=` to a sequence of normalized y-coordinates.
+
+For interval-valued bars, set `data=` to a sequence of normalized `[low, high]` y-coordinates.
+
+
 ```py
 view(
+    # "Column chart":
     box(
         mode='g interval-y',
         style='w-48 h-8 stroke-indigo-700',
         data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 3,
     ),
+    # Interval-valued:
     box(
         mode='g interval-y',
         style='w-48 h-8 stroke-indigo-700',
-        data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+        data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
     ),
 )
 ```
@@ -70,17 +159,33 @@ view(
 
 ## Stroke Y
 
+Set `mode='g stroke-y'` to draw a sequence of vertical strokes. The `stroke-y` mode is similar to the `interval-y`
+mode, except that you can control the thickness of the strokes (bars) when using `stroke-y`.
+
+For simple strokes, set `data=` to a sequence of normalized y-coordinates.
+
+For interval-valued strokes, set `data=` to a sequence of normalized `[low, high]` y-coordinates.
+
+
 ```py
 view(
+    # Strokes:
     box(
         mode='g stroke-y',
         style='w-48 h-8 stroke-indigo-700',
         data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 3,
     ),
+    # Thicker strokes:
+    box(
+        mode='g stroke-y',
+        style='w-48 h-8 stroke-indigo-700 stroke-4',
+        data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 3,
+    ),
+    # Interval-valued:
     box(
         mode='g stroke-y',
         style='w-48 h-8 stroke-indigo-700',
-        data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+        data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
     ),
 )
 ```
@@ -91,17 +196,32 @@ view(
 
 ## Tick Y
 
+Set `mode='g tick-y'` to draw a sequence of horizontal ticks.
+
+For simple ticks, set `data=` to a sequence of normalized y-coordinates.
+
+For interval-valued ticks, set `data=` to a sequence of normalized `[low, high]` y-coordinates.
+
+
 ```py
 view(
+    # Ticks:
     box(
         mode='g tick-y',
         style='w-48 h-8 stroke-indigo-700',
         data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 3,
     ),
+    # Thicker ticks:
+    box(
+        mode='g tick-y',
+        style='w-48 h-8 stroke-indigo-700 stroke-2',
+        data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 3,
+    ),
+    # Interval-valued
     box(
         mode='g tick-y',
         style='w-48 h-8 stroke-indigo-700',
-        data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+        data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
     ),
 )
 ```
@@ -112,11 +232,16 @@ view(
 
 ## Guide X
 
+Set `mode='g guide-x'` to draw a sequence of x-axis guide lines.
+
+Set `data=` to a sequence of normalized x-coordinates.
+
+
 ```py
 view(
     box(
         mode='g guide-x',
-        style='h-8 stroke-indigo-700',
+        style='w-48 h-8 stroke-indigo-700',
         data=[0, 0.2, 0.4, 0.6, 0.8, 0.85, 0.9, 0.95, 0.975, 1],
     ),
 )
@@ -128,11 +253,16 @@ view(
 
 ## Guide Y
 
+Set `mode='g guide-y'` to draw a sequence of y-axis guide lines.
+
+Set `data=` to a sequence of normalized y-coordinates.
+
+
 ```py
 view(
     box(
         mode='g guide-y',
-        style='h-48 w-8 stroke-indigo-700',
+        style='w-8 h-48 stroke-indigo-700',
         data=[0, 0.2, 0.4, 0.6, 0.8, 0.85, 0.9, 0.95, 0.975, 1],
     ),
 )
@@ -143,6 +273,11 @@ view(
 
 
 ## Bar X
+
+Set `mode='g bar-x'` to draw a horizontal bar gauge.
+
+Set `data=` to normalized `[start, end]` values.
+
 
 ```py
 style = 'w-48 h-2 fill-indigo-100 stroke-indigo-700'
@@ -158,6 +293,11 @@ view(
 
 ## Bar Y
 
+Set `mode='g bar-y'` to draw a vertical bar gauge.
+
+Set `data=` to normalized `[start, end]` values.
+
+
 ```py
 style = 'w-2 h-48 fill-indigo-100 stroke-indigo-700'
 view(row(
@@ -172,12 +312,19 @@ view(row(
 
 ## Circle
 
+Set `mode='g circle'` to draw a circular gauge.
+
+Set `data=` to normalized `[start-angle, end-angle, inner-radius, outer-radius]` values.
+
+`inner-radius` and `outer-radius` are optional, and default to `0` and `1` respectively.
+
+
 ```py
 style = 'w-24 h-24 fill-indigo-100 stroke-indigo-700'
 view(row(
     box(mode='g circle', style=style, data=[0, 0.35]),
     box(mode='g circle', style=style, data=[0, 0.35, 0.5, 1]),
-    box(mode='g circle', style=style, data=[0, 0.35, 0.5]),
+    box(mode='g circle', style=style, data=[0, 0.35, 0.5]),  # end-radius defaults to 1.
     box(mode='g circle', style=style, data=[0, 0.35, 0.5, 0.75]),
     box(mode='g circle', style=style, data=[1, 0.35, 0.5, 1]),
 ))
@@ -189,12 +336,19 @@ view(row(
 
 ## Arc
 
+Set `mode='g arc'` to draw a semi-circular gauge.
+
+Set `data=` to normalized `[start-angle, end-angle, inner-radius, outer-radius]` values.
+
+`inner-radius` and `outer-radius` are optional, and default to `0` and `1` respectively.
+
+
 ```py
 style = 'w-24 h-12 fill-indigo-100 stroke-indigo-700'
 view(row(
     box(mode='g arc', style=style, data=[0, 0.35]),
     box(mode='g arc', style=style, data=[0, 0.35, 0.5, 1]),
-    box(mode='g arc', style=style, data=[0, 0.35, 0.5]),
+    box(mode='g arc', style=style, data=[0, 0.35, 0.5]),  # end-radius defaults to 1.
     box(mode='g arc', style=style, data=[0, 0.35, 0.5, 0.75]),
     box(mode='g arc', style=style, data=[1, 0.35, 0.5, 1]),
 ))
--- py/pkg/docs/graphics.py
+++ py/pkg/docs/graphics.py
@@ -19,108 +19,227 @@ from h2o_nitro import View, box, row, col, option, lorem
 # Draw microcharts, sparklines, and other data graphics.
 
 # ## Line Y
+# Set `mode='g line-y'` to draw line and area charts.
+#
+# For a single curve, set `data=` to a sequence of normalized y-coordinates.
+#
+# For dual curves, set `data=` to a sequence of normalized `[low, high]` y-coordinates.
 def graphics_line_y(view: View):  # height 3
-    view(
-        box(
-            mode='g line-y',
-            style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
-            data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 6,
+    view(row(
+        # Single curve:
+        col(
+            # Stroke and fill:
+            box(
+                mode='g line-y',
+                style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
+                data=[0.5, 0.65, 0.5, 0.4, 0.95, 0.05, 0.5, 0.5, 0.6, 0.5, 0.5] * 6,
+            ),
+            # Stroke only:
+            box(
+                mode='g line-y',
+                style='w-48 h-8 fill-none stroke-indigo-700',
+                data=[0.5, 0.65, 0.5, 0.4, 0.95, 0.05, 0.5, 0.5, 0.6, 0.5, 0.5] * 6,
+            ),
+            # Fill only:
+            box(
+                mode='g line-y',
+                style='w-48 h-8 fill-indigo-700 stroke-none',
+                data=[0.5, 0.65, 0.5, 0.4, 0.95, 0.05, 0.5, 0.5, 0.6, 0.5, 0.5] * 6,
+            ),
         ),
-        box(
-            mode='g line-y',
-            style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
-            data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+        # Dual curve:
+        col(
+            # Stroke and fill:
+            box(
+                mode='g line-y',
+                style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
+                data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+            ),
+
+            # Stroke only:
+            box(
+                mode='g line-y',
+                style='w-48 h-8 fill-none stroke-indigo-700',
+                data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+            ),
+            # Fill only:
+            box(
+                mode='g line-y',
+                style='w-48 h-8 fill-indigo-700 stroke-none',
+                data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+            ),
         ),
-    )
+    ))
 
 
 # ## Step Y
+# Set `mode='g step-y'` to draw step charts. Step charts are similar to line charts, except that adjacent points are
+# connected using discrete steps instead of line segments.
+#
+# For a single curve, set `data=` to a sequence of normalized y-coordinates.
+#
+# For dual curves, set `data=` to a sequence of normalized `[low, high]` y-coordinates.
 def graphics_step_y(view: View):  # height 3
-    view(
-        box(
-            mode='g step-y',
-            style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
-            data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 6,
+    view(row(
+        # Single curve:
+        col(
+            # Stroke and fill:
+            box(
+                mode='g step-y',
+                style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
+                data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 6,
+            ),
+            # Stroke only:
+            box(
+                mode='g step-y',
+                style='w-48 h-8 fill-none stroke-indigo-700',
+                data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 6,
+            ),
+            # Fill only:
+            box(
+                mode='g step-y',
+                style='w-48 h-8 fill-indigo-700 stroke-none',
+                data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 6,
+            ),
         ),
-        box(
-            mode='g step-y',
-            style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
-            data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+        # Dual curve:
+        col(
+            # Stroke and fill:
+            box(
+                mode='g step-y',
+                style='w-48 h-8 fill-indigo-100 stroke-indigo-700',
+                data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
+            ),
+            # Stroke only:
+            box(
+                mode='g step-y',
+                style='w-48 h-8 fill-none stroke-indigo-700',
+                data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
+            ),
+            # Fill only:
+            box(
+                mode='g step-y',
+                style='w-48 h-8 fill-indigo-700 stroke-none',
+                data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
+            ),
         ),
-    )
+    ))
 
 
 # ## Interval Y
+# Set `mode='g interval-y'` to draw bar/column charts.
+#
+# For simple bars, set `data=` to a sequence of normalized y-coordinates.
+#
+# For interval-valued bars, set `data=` to a sequence of normalized `[low, high]` y-coordinates.
 def graphics_interval_y(view: View):  # height 3
     view(
+        # "Column chart":
         box(
             mode='g interval-y',
             style='w-48 h-8 stroke-indigo-700',
             data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 3,
         ),
+        # Interval-valued:
         box(
             mode='g interval-y',
             style='w-48 h-8 stroke-indigo-700',
-            data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+            data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
         ),
     )
 
 
 # ## Stroke Y
+# Set `mode='g stroke-y'` to draw a sequence of vertical strokes. The `stroke-y` mode is similar to the `interval-y`
+# mode, except that you can control the thickness of the strokes (bars) when using `stroke-y`.
+#
+# For simple strokes, set `data=` to a sequence of normalized y-coordinates.
+#
+# For interval-valued strokes, set `data=` to a sequence of normalized `[low, high]` y-coordinates.
 def graphics_stroke_y(view: View):  # height 3
     view(
+        # Strokes:
         box(
             mode='g stroke-y',
             style='w-48 h-8 stroke-indigo-700',
             data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 3,
         ),
+        # Thicker strokes:
+        box(
+            mode='g stroke-y',
+            style='w-48 h-8 stroke-indigo-700 stroke-4',
+            data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 3,
+        ),
+        # Interval-valued:
         box(
             mode='g stroke-y',
             style='w-48 h-8 stroke-indigo-700',
-            data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+            data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
         ),
     )
 
 
 # ## Tick Y
+# Set `mode='g tick-y'` to draw a sequence of horizontal ticks.
+#
+# For simple ticks, set `data=` to a sequence of normalized y-coordinates.
+#
+# For interval-valued ticks, set `data=` to a sequence of normalized `[low, high]` y-coordinates.
 def graphics_tick_y(view: View):  # height 3
     view(
+        # Ticks:
         box(
             mode='g tick-y',
             style='w-48 h-8 stroke-indigo-700',
             data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 3,
         ),
+        # Thicker ticks:
+        box(
+            mode='g tick-y',
+            style='w-48 h-8 stroke-indigo-700 stroke-2',
+            data=[0.1, 0.9, 0.2, 0.8, 0.3, 0.7, 0.4, 0.6, 0.5] * 3,
+        ),
+        # Interval-valued
         box(
             mode='g tick-y',
             style='w-48 h-8 stroke-indigo-700',
-            data=[[0.5, 0.8], [0.2, 0.6], [0.3, 0.9], [0.6, 0.7]] * 6,
+            data=[[0.1, 0.9], [0.2, 0.8], [0.3, 0.7], [0.4, 0.6]] * 6,
         ),
     )
 
 
 # ## Guide X
+# Set `mode='g guide-x'` to draw a sequence of x-axis guide lines.
+#
+# Set `data=` to a sequence of normalized x-coordinates.
 def graphics_guide_x(view: View):  # height 3
     view(
         box(
             mode='g guide-x',
-            style='h-8 stroke-indigo-700',
+            style='w-48 h-8 stroke-indigo-700',
             data=[0, 0.2, 0.4, 0.6, 0.8, 0.85, 0.9, 0.95, 0.975, 1],
         ),
     )
 
 
 # ## Guide Y
+# Set `mode='g guide-y'` to draw a sequence of y-axis guide lines.
+#
+# Set `data=` to a sequence of normalized y-coordinates.
 def graphics_guide_y(view: View):  # height 3
     view(
         box(
             mode='g guide-y',
-            style='h-48 w-8 stroke-indigo-700',
+            style='w-8 h-48 stroke-indigo-700',
             data=[0, 0.2, 0.4, 0.6, 0.8, 0.85, 0.9, 0.95, 0.975, 1],
         ),
     )
 
 
 # ## Bar X
+# Set `mode='g bar-x'` to draw a horizontal bar gauge.
+#
+# Set `data=` to normalized `[start, end]` values.
 def graphics_bar_x(view: View):  # height 3
     style = 'w-48 h-2 fill-indigo-100 stroke-indigo-700'
     view(
@@ -130,6 +249,9 @@ def graphics_bar_x(view: View):  # height 3
 
 
 # ## Bar Y
+# Set `mode='g bar-y'` to draw a vertical bar gauge.
+#
+# Set `data=` to normalized `[start, end]` values.
 def graphics_bar_y(view: View):  # height 3
     style = 'w-2 h-48 fill-indigo-100 stroke-indigo-700'
     view(row(
@@ -139,24 +261,34 @@ def graphics_bar_y(view: View):  # height 3
 
 
 # ## Circle
+# Set `mode='g circle'` to draw a circular gauge.
+#
+# Set `data=` to normalized `[start-angle, end-angle, inner-radius, outer-radius]` values.
+#
+# `inner-radius` and `outer-radius` are optional, and default to `0` and `1` respectively.
 def graphics_circle(view: View):  # height 3
     style = 'w-24 h-24 fill-indigo-100 stroke-indigo-700'
     view(row(
         box(mode='g circle', style=style, data=[0, 0.35]),
         box(mode='g circle', style=style, data=[0, 0.35, 0.5, 1]),
-        box(mode='g circle', style=style, data=[0, 0.35, 0.5]),
+        box(mode='g circle', style=style, data=[0, 0.35, 0.5]),  # end-radius defaults to 1.
         box(mode='g circle', style=style, data=[0, 0.35, 0.5, 0.75]),
         box(mode='g circle', style=style, data=[1, 0.35, 0.5, 1]),
     ))
 
 
 # ## Arc
+# Set `mode='g arc'` to draw a semi-circular gauge.
+#
+# Set `data=` to normalized `[start-angle, end-angle, inner-radius, outer-radius]` values.
+#
+# `inner-radius` and `outer-radius` are optional, and default to `0` and `1` respectively.
 def graphics_arc(view: View):  # height 3
     style = 'w-24 h-12 fill-indigo-100 stroke-indigo-700'
     view(row(
         box(mode='g arc', style=style, data=[0, 0.35]),
         box(mode='g arc', style=style, data=[0, 0.35, 0.5, 1]),
-        box(mode='g arc', style=style, data=[0, 0.35, 0.5]),
+        box(mode='g arc', style=style, data=[0, 0.35, 0.5]),  # end-radius defaults to 1.
         box(mode='g arc', style=style, data=[0, 0.35, 0.5, 0.75]),
         box(mode='g arc', style=style, data=[1, 0.35, 0.5, 1]),
     ))
